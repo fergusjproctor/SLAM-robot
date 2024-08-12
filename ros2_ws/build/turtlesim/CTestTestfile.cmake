594c59f2367cfca4e9babcd879ee334efdad4a2e
Fergus Proctor <fergusjproctor@gmail.com>
2024-08-12 17:21:28 +0200

    Added, installed and tested turtle_sim node

---
+++ ros2_ws/build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/fergusproctor/dev/SLAM-robot/ros2_ws/src/turtlesim
+# Build directory: /Users/fergusproctor/dev/SLAM-robot/ros2_ws/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
